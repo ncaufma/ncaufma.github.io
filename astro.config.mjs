24616eaaa08467a721ecbcb952a7ff965faee837
NCA UFMA <nca@nca.ufma.br>
2026-04-28 19:11:42 -0300

    Remove base path - repo renomeado para ncaufma.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ncaufma.github.io',
-  base: '/nca.ufma.github.io',
   output: 'static',
   integrations: [],
   build: {
